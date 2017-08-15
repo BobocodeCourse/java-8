06c8bf523fe12f77959fd50df4e67e4755fcae57
Serhii Pivenko <spivenko@playtika.com>
2017-08-15 21:40:32 +0300

    Stream Task Collecting
    Fix

--- stream-api/src/main/java/com/bobocode/StreamTask_Collecting.java
+++ stream-api/src/main/java/com/bobocode/StreamTask_Collecting.java
@@ -4,7 +4,9 @@ package com.bobocode;
 import com.bobocode.model.Account;
 import com.bobocode.util.TestDataProvider;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Using Stream API, lambdas, and predefined functional interfaces implement the code according to requirements
@@ -14,15 +16,53 @@ public class StreamTask_Collecting {
         List<Account> accounts = TestDataProvider.generateAccountList();
 
         // 1 - Find a set of account owner's names(first name) that have a length <= 4
+        accounts.stream()
+                .filter(a -> a.getFirstName().length() <= 4)
+                .collect(Collectors.toSet())
+                .forEach(a -> System.out.println(a.getFirstName()));
+
+        System.out.println("--------------------------------");
 
         // 2 - Group all accounts by its email providers. (You can consider as an email provider
         // everything that goes after @ sign
 
+        accounts.stream()
+                .collect(Collectors.groupingBy(a -> a.getEmail().split("@")[1]))
+                .entrySet()
+                .stream()
+                .forEach(System.out::println);
+
+        System.out.println("--------------------------------");
+
         // 3 - Find the richest guy
+
+        Optional<Account> theRichestGuy = accounts.stream()
+                .collect(Collectors.maxBy(Comparator.comparing(Account::getBalance)));
+
+        System.out.println(theRichestGuy);
+
+        Optional<Account> theRichestGuy2 = accounts.stream()
+                .max(Comparator.comparing(Account::getBalance));
+
+        System.out.println(theRichestGuy2);
+
+        System.out.println("--------------------------------");
 
         // 4 - Find the richest guy for each email provider
 
+        accounts.stream()
+                .collect(Collectors.groupingBy(a -> a.getEmail().split("@")[1],
+                        Collectors.maxBy(Comparator.comparing(Account::getBalance))))
+                .entrySet()
+                .forEach(System.out::println);
+
+        System.out.println("--------------------------------");
+
         // 5 - Split accounts by their balance (those who have more than $90 000.00, and those who don't)
 
+        accounts.stream()
+                .collect(Collectors.partitioningBy(a -> a.getBalance().compareTo(BigDecimal.valueOf(90000.00)) == 1))
+                .entrySet()
+                .forEach(System.out::println);
     }
 }
