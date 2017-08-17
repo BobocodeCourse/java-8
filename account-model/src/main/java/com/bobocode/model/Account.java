e0570e3736896fec0cea8d4a9735189ba81c513c
hamster <oleksii.skachkov@gmail.com>
2017-08-17 07:14:35 +0300

    добавил реализацию класса Account

--- account-model/src/main/java/com/bobocode/model/Account.java
+++ account-model/src/main/java/com/bobocode/model/Account.java
@@ -5,11 +5,11 @@ import lombok.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
-@Getter
-@Setter
-@ToString
+//@NoArgsConstructor
+//@AllArgsConstructor(access = AccessLevel.PUBLIC)
+//@Getter
+//@Setter
+//@ToString
 public class Account {
     private Long id;
     private String firstName;
@@ -18,4 +18,83 @@ public class Account {
     private LocalDate birthday;
     private LocalDate creationDate;
     private BigDecimal balance = BigDecimal.ZERO;
+
+    public Account(Long id, String firstName, String lastName, String email, LocalDate birthday, LocalDate creationDate, BigDecimal balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.birthday = birthday;
+        this.creationDate = creationDate;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", birthday=" + birthday +
+                ", creationDate=" + creationDate +
+                ", balance=" + balance +
+                '}';
+    }
 }
