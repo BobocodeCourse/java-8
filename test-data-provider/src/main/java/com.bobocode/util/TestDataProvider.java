b542175e9507a344d2e971aa27d32806dfcbe615
Denys Tymoshkevych <dtimoshkevich@egp.com.ua>
2017-08-18 12:00:11 +0300

    add resolved task _Refactoring_

--- test-data-provider/src/main/java/com.bobocode/util/TestDataProvider.java
+++ test-data-provider/src/main/java/com.bobocode/util/TestDataProvider.java
@@ -18,17 +18,17 @@ public class TestDataProvider {
     public static List<Account> generateAccountList() {
         List<Account> accounts = new ArrayList<>(6);
         accounts.add(new Account(1L, "Will", "Smith", "wsmith@gmail.com",
-                LocalDate.of(1968, 8, 25), LocalDateTime.now(), BigDecimal.valueOf(104065.25)));
+                LocalDate.of(1968, 8, 25), LocalDateTime.of(2010, 5, 5, 5, 5), BigDecimal.valueOf(104065.25)));
         accounts.add(new Account(2L, "Tom", "Hanks", "thanks@gmail.com",
-                LocalDate.of(1956, 7, 9), LocalDateTime.now(), BigDecimal.valueOf(93065.25)));
+                LocalDate.of(1956, 7, 9), LocalDateTime.of(2011, 5, 5, 5,5), BigDecimal.valueOf(93065.25)));
         accounts.add(new Account(3L, "Russell", "Crowe", "rcrowe@gmail.com",
-                LocalDate.of(1964, 4, 7), LocalDateTime.now(), BigDecimal.valueOf(87641.98)));
+                LocalDate.of(1964, 4, 7), LocalDateTime.of(2012, 5, 5, 5,5), BigDecimal.valueOf(87641.98)));
         accounts.add(new Account(4L, "Robert", "Downey", "rdowney@yahoo.com",
-                LocalDate.of(1965, 4, 9), LocalDateTime.now(), BigDecimal.valueOf(152345)));
+                LocalDate.of(1965, 4, 9), LocalDateTime.of(2013, 1, 5, 5,5), BigDecimal.valueOf(152345)));
         accounts.add(new Account(5L, "Robert", "De Niro", "deniro@outlook.com",
-                LocalDate.of(1943, 8, 17), LocalDateTime.now(), BigDecimal.valueOf(67065.45)));
+                LocalDate.of(1943, 8, 17), LocalDateTime.of(2016, 5, 5, 5,5), BigDecimal.valueOf(67065.45)));
         accounts.add(new Account(6L, "Metthew", "Perry", "mperry@gmail.com",
-                LocalDate.of(1969, 8, 19), LocalDateTime.now(), BigDecimal.valueOf(45678.12)));
+                LocalDate.of(1969, 8, 19), LocalDateTime.of(2017, 5, 5, 5,5), BigDecimal.valueOf(45678.12)));
         return accounts;
     }
 
